a52fed432d5ecffe8ca9e26cc44361c8ee019547
Kenneth Aduan <kenneth.aduan10@gmail.com>
2023-03-23 07:30:21 +0800

    Splash screen Added fake frend

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -7,20 +7,11 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-        Handler h = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        h.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this,Splash.class);
-              startActivity(i);
-              finish();
-
-            }
-        },5000);
     }
 }
